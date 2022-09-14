b5bd99204de2482bb6a46216dd5e04968152c7cf
Lucas Aprigio <lucsaprigio@hotmail.com>
2022-09-14 07:47:05 -0300

    Web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
 import './styles/main.css';
 
 import logo from './assets/Logo.svg'
@@ -61,7 +63,18 @@ function App() {
         </a>
       </div>
 
-      <div className="bg-[#2A2634] px-8 py-6 mt-8 rounded-lg self-stretch before:w-full before:h-2">
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400">Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
 
       </div>
     </div>
