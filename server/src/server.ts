592b163cbc0c6924406b26ae52f38257c7d5f0da
Lucas Aprigio <lucsaprigio@hotmail.com>
2022-09-15 08:22:36 -0300

    Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/ads', (req, res) => {
-    return res.json({ message: "Hello World !"})
+app.get('/games', (req, res) => {
+    const games = prisma.game.findMany()
+
+    return res.json([]);
 });
+
+app.get('/games/:id/ads', (req, res) => {
+    return res.json({ message: "Hello World !" })
+});
+
+app.get('/games/:id/discord', (req, res) => {
+    return res.json({ message: "Hello World !" })
+});
+
+app.post('/ads', (req, res) => {
+    return res.json([]);
+});
+
 
 app.listen(3333);
 
