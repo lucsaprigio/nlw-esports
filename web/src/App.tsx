f2697f177133c41dd0c728d55e7580884dde4540
Lucas Aprigio <lucsaprigio@hotmail.com>
2022-09-20 16:09:40 -0300

    Finalized �

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import * as Diolag from '@radix-ui/react-dialog';
 import { GameBanner } from './components/GameBanner';
-import { GameController } from 'phosphor-react';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
 import './styles/main.css';
 
 import logo from './assets/Logo.svg'
-import { Input } from './components/Form/Input';
+import { CreateAdModal } from './components/CreateAdModal';
 
 interface Game {
   id: string;
@@ -22,19 +22,16 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/games ')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data)
-        console.log(data)
-      })
+    axios('http://localhost:3333/games ').then(response => {
+      setGames(response.data);
+    })
   }, []);
 
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="logo" />
       <h1 className="text-6xl text-white font-black mt-20" >
-        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span>está aqui
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
@@ -50,116 +47,7 @@ function App() {
       </div>
       <Diolag.Root>
         <CreateAdBanner />
-
-        <Diolag.Portal>
-          <Diolag.Overlay className="bg-black/60 inset-0 fixed" />
-
-          <Diolag.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-            <Diolag.Title className="text-3xl font-black">Publique um anúncio</Diolag.Title>
-
-            <form className="mt-8 flex flex-col gap-4">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="game" className="font-semibold">Qual o game?</label>
-                <Input id="game" placeholder="Selecione o game que deseja jogar" />
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <label htmlFor="name">Seu nome (ou nickname)</label>
-                <Input id="name" type="text" placeholder="Como te chamam dentro do game" />
-              </div>
-
-              <div className="grid grid-cols-2 gap-6">
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                  <Input id="yearsPlaying" type="number" placeholder="Tudo bem ser ZERO" />
-                </div>
-
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="discord">Qual o seu Discord?</label>
-                  <Input id="discord" type="text" placeholder="Usuário#0000" />
-                </div>
-              </div>
-
-              <div className="flex gap-6">
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="weekDays">Quando costuma jogar?</label>
-
-                  <div className="grid grid-cols-4 gap-4">
-                    <button
-                      title="Domingo"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      D
-                    </button>
-                    <button
-                      title="Segunda"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      S
-                    </button>
-                    <button
-                      title="Terça"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      T
-                    </button>
-                    <button
-                      title="Quarta"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      Q
-                    </button>
-                    <button
-                      title="Quinta"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      Q
-                    </button>
-                    <button
-                      title="Sexta"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      S
-                    </button>
-                    <button
-                      title="Sábado"
-                      className="w-8 h8 rounded bg-zinc-900 "
-                    >
-                      S
-                    </button>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-2 flex-1">
-                  <label htmlFor="hourStart">Qual horário do dia?</label>
-                  <div className="grid grid-cols-2 gap-2">
-                    <Input id="hourStart" type="time" placeholder="De" />
-                    <Input id="hourEnd" type="time" placeholder="Até" />
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-2 flex-gap-2 text-sm">
-                <Input type="checkbox" />
-                Costumo me conectar ao chat de voz
-              </div>
-
-              <footer className="mt-4 flex justify-end gap-4">
-                <Diolag.Close
-                  type="button"
-                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
-                >Cancelar
-                </Diolag.Close>
-                <button
-                  type="submit"
-                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
-                >
-                  <GameController className="w-6 h-6" />
-                  Encontrar duo
-                </button>
-              </footer>
-            </form>
-          </Diolag.Content>
-        </Diolag.Portal>
+        <CreateAdModal />
       </Diolag.Root>
     </div>
   )
